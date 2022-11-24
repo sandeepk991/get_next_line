cbd825f28f66666d899e5ca5f5e97b997bf9197f
Sandeep Kaur <skaur@c2r1p9.42vienna.com>
2022-11-24 15:05:24 +0100

    nov 24 15:05 updated

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,69 @@
 /*   By: skaur <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 12:41:46 by skaur             #+#    #+#             */
-/*   Updated: 2022/11/24 13:02:21 by skaur            ###   ########.fr       */
+/*   Updated: 2022/11/24 13:41:49 by skaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
 
+	len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}		
+	return (len);
+}
+
+char	*ft_strdup(const char *s)
+{
+	size_t	s_len;
+	char	*result;
+
+	s_len = ft_strlen(s);
+	result = (char *)malloc(sizeof(char) * (s_len + 1));
+	if (result == NULL)
+		return (0);
+	s_len = 0;
+	while (s[s_len])
+	{
+		result[s_len] = s[s_len];
+		s_len++;
+	}
+	result[s_len] = '\0';
+	return (result);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*newstr;
+	int		i;
+	int		j;
+
+	newstr = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (newstr == NULL)
+		return (0);
+	i = 0;
+	j = 0;
+	while (s1[i])
+		newstr[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		newstr[j++] = s2[i++];
+	newstr[j] = '\0';
+	return (newstr);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != (unsigned char) c)
+	{
+		if (*s == '\0')
+			return (0);
+		s++;
+	}
+	return ((char *)s);
+}
